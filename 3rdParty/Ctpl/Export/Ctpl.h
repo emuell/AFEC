c1d1aa1ff58e2e4a3afe21503ff54ee0deadf000
Eduard Müller <mail@emuell.net>
2021-08-18 15:05:57 +0200

    initial import of CTPL (Modern and efficient C++ Thread Pool Library)

---
+++ 3rdParty/Ctpl/Export/Ctpl.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef _Ctpl_h_
+#define _Ctpl_h_
+
+#include "CoreTypes/Export/CompilerDefines.h"
+
+// =================================================================================================
+
+#if defined(MCompiler_VisualCPP)
+  #define BOOST_ALL_NO_LIB
+#endif
+
+// =================================================================================================
+
+#include "../../3rdParty/Ctpl/Dist/ctpl.h"
+
+
+#endif // _Ctpl_h_
+
